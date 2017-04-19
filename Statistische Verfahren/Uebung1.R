20d7c744cfdf19c40498eeb6a9eeff884f74812e
Jonas Aaron Gütter <xi86xev>
2017-04-19 12:10:08 +0200

    added Uebung1

---
+++ Statistische Verfahren/Uebung1.R
@@ -0,0 +1,7 @@
+## Übungsblatt 1
+
+
+directory = "D:/CDS/Statistische_Verfahren/"
+filename = "2017_SS_7_2_3_soilrespiration1.csv"
+d = read.csv("D:/CDS/Statistische_Verfahren/2017_SS_7_2_3_soilrespiration1.csv")
+
