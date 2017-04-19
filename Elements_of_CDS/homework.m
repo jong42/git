986542997ff972e8d6a552504628a3dad171f803
Jonas Aaron Gütter <xi86xev@ppc811.mirz.uni-jena.de>
2017-04-19 10:37:10 +0200

    continued script

--- Elements_of_CDS/homework.m
+++ Elements_of_CDS/homework.m
@@ -1,17 +1,19 @@
-d = csvread ('Data3.csv',2,0)
 
-plot (d,':');
-xlabel ('weeks');
-ylabel ('number of influenca cases');
-title ('process of influenca in season 2016/17');
-xlim ([0 30])
+% d = csvread ('Data3.csv',2,0)
+% 
+% plot (d,':');
+% xlabel ('weeks');
+% ylabel ('number of influenca cases');
+% title ('process of influenca in season 2016/17');
+% xlim ([0 30])
 
- tspan = [0 10]
- y0 = [1000,10,0]
- alpha = 0.5
- beta = 0.1
+ tspan = [0 2];
+ y0 = [1000,10,0];
+ alpha = 0.01;
+ beta = 0.1;
  func = @(t,x)SIRMODEL(t,x,alpha,beta);
- %[t,y] = ode23 (func,tspan,y0)
+ [t,y] = ode23 (func,tspan,y0)
 
 
 
+plot (y,t)
