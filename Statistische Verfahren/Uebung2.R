0925f9ff3a8b1472f7811ea62213ce6f46480bc0
Jonas Aaron Gütter <xi86xev@ppc811.mirz.uni-jena.de>
2017-04-26 16:38:40 +0200

    continued Uebung2

--- Statistische Verfahren/Uebung2.R
+++ Statistische Verfahren/Uebung2.R
@@ -17,22 +17,47 @@ pnorm(mu+3*sigma,mu, sigma) - pnorm(mu-3*sigma,mu, sigma)
 ########################
 ### 2.
 ########################
+library(stats)
+
 
 E = 0.5
 var = (0.5)^2
 
+### k=5
 k = 5
+
 
 y = sum(runif(k))/k
 for (i in 1:999) {
   new_element = sum(runif(k))/k
   y = c(y,new_element)}
 
+hist(y)
+plot(y,dnorm(y,E,var))
+distribution = ecdf(y)
+
+
+### k=10
+k = 10
+
+y = sum(runif(k))/k
+for (i in 1:999) {
+  new_element = sum(runif(k))/k
+  y = c(y,new_element)}
+
+hist(y)
+plot(y,dnorm(y,E,var))
+distribution = ecdf(y)
+
+
+
+
 ########################
 ### 3.
 ########################
 
-x = read.csv()
+d = read.csv("/home/xi86xev/git/git/Statistische Verfahren/2017_SS_7_2_3_soilrespiration1.csv", sep = ";")
+
 
 
   
