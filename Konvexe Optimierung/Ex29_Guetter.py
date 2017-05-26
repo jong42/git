aef8acabbdbf74da2179ee9fdaa795c68e127c69
Jonas Aaron Gütter <xi86xev@ppc819.mirz.uni-jena.de>
2017-05-26 19:17:01 +0200

    continued script

--- Konvexe Optimierung/Ex29_Guetter.py
+++ Konvexe Optimierung/Ex29_Guetter.py
@@ -1,52 +1,79 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def gradientenverfahren_armijo(Q,q,epsilon,delta, beta):
-	# Setze Startpunkt und Z?hler
+def gradientenverfahren_armijo(func,gradient,epsilon,delta,beta):
+	# Setze Startpunkt und Zaehler
 	k = 0
 	finish = False
 	x = np.array([[1],[1]])
 	while (finish == False):
-		# Berechne Gradient
-		gradient = (Q.dot(x))+q
-		#print (gradient)
-		# Pr?fe, ob Minimum bereits erreicht ist
-		if (not np.any(gradient)):
+		# Pruefe, ob Minimum bereits erreicht ist
+		if (not np.any(gradient(x))):
 			break
-		# Pr?fe, ob Abbruchkriterium bereits erf?llt ist
-		if (np.linalg.norm(gradient)<epsilon):
+		# Pruefe, ob Abbruchkriterium bereits erfuellt ist
+		if (np.linalg.norm(gradient(x))<epsilon):
 			break
-		# F?ge zus?tzliche Abbruchkriterien hinzu
-		if (k>10000):
+		# Fuege zusaetzliche Abbruchkriterien hinzu
+		if (k>1000):
 			break
 		# Berechne Armijo-Schrittweite
-		d = - gradient
+		d = - gradient(x)
 		sigma = 1
 		finish_2 = False
 		while (finish_2 == False):
-        	# Pr?fe, ob Bedingung erf?llt ist
+        	# Pruefe, ob Bedingung erfuellt ist
 			x_new = x+sigma*d
-			# WAHRSCHEINLICH HIER EIN FEHLER: SCHLEIFE BRICHT NICHT AB
-        	if(0.5*((x_new.T).dot(Q)).dot(x_new)+(q.T).dot(x_new) <= 0.5*((x.T).dot(Q)).dot(x)+(q.T).dot(x)+delta*sigma*gradient.T.dot(d)):
+			if(func(x_new) <= func(x)+(delta*sigma*(gradient(x).T)).dot(d)):
 				break
-        	sigma = beta*sigma
-		# Berechne n?chsten Versuchs-Punkt
-        x = x + sigma*d
-		# inkrementiere Z?hlvariable
-        k+=1
+			sigma = beta*sigma
+		# Berechne naechsten Versuchs-Punkt
+		x = x + sigma*d
+		# inkrementiere Zaehlvariable
+		k+=1
 
-	print(k)
+	#print("k:"+str(k))
 	return x;
 
+def quadratic_func(x):
+	Q = np.array([[1,2], [2,1]])
+	q = np.array([[2],[2]])
+	c = 2
+	res = 0.5* float(((x.T).dot(Q).dot(x))+ (q.T).dot(x)+c)
+	return res;
+
+def gradient(x):
+	Q = np.array([[1,2], [2,1]])
+	q = np.array([[2],[2]])
+	res = Q.dot(x)+q
+	return res;
 
 def main():
 
-	Q = np.array([[1,2], [2,1]])
-	q = np.array([[2],[2]])
-	epsilon = 0.001
+
+	epsilon = 0.01
 	delta = 0.01
 	beta = 0.5
-	print (gradientenverfahren_armijo(Q,q,epsilon, delta, beta))
+	
+	plt.figure()
 
+	x = np.arange(-10,10,1)
+	y = np.arange(-10,10,1)
+	X, Y = np.meshgrid(x, y)
+	z = []
+	
+	for i in range(0,20):
+		z.append (quadratic_func(np.array([[x[i]],[y[i]]])))
+	
+	Z = np.meshgrid(x,z)
+	print (Z.ndim)
+	#plt.contourf(X,Y,Z)
+
+	#t = np.arange(-10,10,1)
+	#qt = np.array([[t],[t]])
+	#plt.plot(qt,quadratic_func(qt).astype(np.int))
+	#plt.show()
+	#print (gradientenverfahren_armijo(quadratic_func,gradient,epsilon, delta, beta))
+	
 
 if __name__ == '__main__':
     main()
