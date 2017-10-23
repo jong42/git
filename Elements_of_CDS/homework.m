11aa2a8550d3a6756fec8de8f85d9d43e86ea237
Jonas Aaron Gütter <xi86xev@ppc667.mirz.uni-jena.de>
2017-10-23 11:35:44 +0200

    message

--- Elements_of_CDS/homework.m
+++ Elements_of_CDS/homework.m
@@ -10,10 +10,13 @@
  tspan = [0 2];
  y0 = [1000,10,0];
  alpha = 0.01;
- beta = 0.1;
+ beta = 0.8;
  func = @(t,x)SIRMODEL(t,x,alpha,beta);
  [t,y] = ode23 (func,tspan,y0)
 
 
 
-plot (y,t)
+plot (t,y)
+xlabel ('time')
+ylabel ('number of influenca cases')
+legend ('Susceptible','Ill','Cured')
