1afa8e92b50dfce12986c444328a8512df1b8c42
Jonas Aaron Gütter <xi86xev@ppc668.mirz.uni-jena.de>
2017-11-21 16:08:35 +0100

    WissrechB1A3

--- Wissenschaftliches Rechnen 1/Blatt1_A3.R
+++ Wissenschaftliches Rechnen 1/Blatt1_A3.R
@@ -1,21 +1,59 @@
 library(Matrix)
 
-## x and y have to be between 0 and 1, h<=1
+################################################
+#####Erstellen des Gleichungssystems Ax=b
+################################################
+
 create_band_matrix = function(h){
   main_diag = seq(4,4,length.out = 1/h**2)
   side_diag = seq(-1,-1,length.out = 1/h**2)
-  A_tridiag = bandSparse(n=as.integer(1/h**2),m=as.integer(1/h**2),k=c(-1,0,1,1/h,-1/h), diagonals=list(side_diag,main_diag,side_diag,side_diag,side_diag))
+  A_tridiag = bandSparse(n=as.integer(1/h**2)+1,m=as.integer(1/h**2)+1,k=c(-1,0,1,1/h,-1/h), diagonals=list(side_diag,main_diag,side_diag,side_diag,side_diag))
   return (A_tridiag)
 }
 
 create_b_vector = function(h){
   b = vector(length = 1/h**2)
-  return(b)
-  for(x in 0,1,by=1/h)
+  for(x in seq(0.1,1,by=h)){
+    for (y in seq(0.1,1,by=h)){
+      b[x/h**2 + y/h] = x*(1-x)+y*(1-y)
+    }
+  }
+  return(b[-(1:9)])
 }
 
 A = create_band_matrix(0.1)
 b = create_b_vector(0.1)
 
-laplace_operator = - (x*(1-x)+y*(1-y))
-u_rand = 0
+################################################
+##### LU-Faktorisierung
+################################################
+h = 0.1
+
+
+perform_LU_fact = function(h,A) {
+# Perform LR-Partitioning
+  n = 100
+  R = A
+  L = diag(1,n,n)
+
+  # n-1 Iterationsschritte
+  for (i in seq(1,n-1)) {
+    print(paste0("Step: ", i))
+    for(k in seq(i+1,n)) {
+      #
+      # Berechnung von L
+      L[k,i] = R[k,i] / R[i,i]
+      #print(L)
+      # Spalten der Restmatrix werden durchlaufen
+      for (j in seq(i,n)) {
+        #Berechnung von R
+        print(k)
+        R[k,j] = R[k,j] - L[k,i] * R[i,j]
+        #print(R)
+      } 
+    }
+  }
+  return (list("L" = L,"R" = R))
+}
+
+perform_LU_fact(h,A)
