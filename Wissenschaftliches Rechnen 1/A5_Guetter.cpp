d28a278335f114aea417947809a68736b32b36b6
Jonas Aaron Gütter <xi86xev@ppc662.mirz.uni-jena.de>
2017-11-27 20:52:02 +0100

    wissrech

--- Wissenschaftliches Rechnen 1/A5_Guetter.cpp
+++ Wissenschaftliches Rechnen 1/A5_Guetter.cpp
@@ -1,31 +1,66 @@
 #include <iostream>
-#include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <cmath>
+#include <fstream>
+#include <string>
+
+std::vector<float> gs(int n, float v, int iter){
+	// Initialize variables
+	float h =(float)1/(n+1);
+	std::vector<float> b((n+2)*(n+2));
+	for(int i=0; i<n*n; i++){
+		b[i] = 1;
+	}
+	float v_x = v/sqrt(5);
+	float v_y = 2*v/(sqrt(5));
+	float a_diag = 4/(h*h);
+	float a_left = -1/(h*h) -v_x/(2*h);
+	float a_right = -1/(h*h) +v_x/(2*h);
+	float a_up = -1/(h*h) + v_y/2*h;
+	float a_down = -1/(h*h) - v_y/2*h;
+	std::vector<float> u((n+2)*(n+2),0.0);
+	std::vector<float> x((n+2)*(n+2),0.0);
+	// Begin iterations
+	for (int w=0;w<iter;w++){
+		for (int i=1;i<n+1;i++){
+			for (int j=1;j<n+1;j++){
+
+				u[i*(n+2)+j] = (b[i*(n+2)+j]-(a_down*u[(i-1)*(n+2)+j]+a_left*u[i*(n+2)+j-1]+a_right*u[i*(n+2)+j+1]+a_up*u[(i+1)*(n+2)+j]))/a_diag;
+			}
+			//std::cout << x[i] << std::endl;
+		}
+		// set start vector for next iteration
+
+	}
+	return u;
+}
+
 
 std::vector<float> jacobi(int n, float v, int iter){
 	// Initialize variables
-	float h =1/(n+1);
+	float h =(float)1/(n+1);
 	std::vector<float> b(n*n);
 	for(int i=0; i<n*n; i++){
 		b[i] = 1;
 	}
 	float v_x = v/sqrt(5);
-	float v_y = v/(2*sqrt(5));
-	float a_diag = 4/pow(h,2) - v_x/h - v_y/h;
-	float a_lr = -1/pow(h,2) + v_x/h;
-	float a_ud = -1/pow(h,2) + v_y/h;
-	std::vector<float> u(n*n);
-	std::vector<float> x(n*n);
-	// Initialize start vector u
-	for(int i=0; i<n; i++){
-		u[i] = 1;
-	}
+	float v_y = 2*v/(sqrt(5));
+	float a_diag = 4/(h*h);
+	float a_left = -1/(h*h) -v_x/(2*h);
+	float a_right = -1/(h*h) +v_x/(2*h);
+	float a_up = -1/(h*h) + v_y/2*h;
+	float a_down = -1/(h*h) - v_y/2*h;
+	std::vector<float> u((n+2)*(n+2),0.0);
+	std::vector<float> x((n+2)*(n+2),0.0);
 	// Begin iterations
 	for (int w=0;w<iter;w++){
-		std::cout << "hello there" << std::endl;
-		for (int i=n;i<(n*(n-1));i++){
-			x[i] = b[i]- (a_ud*u[i-n]+a_lr*u[i-1]+a_lr*u[i+1]+a_ud*u[i+n])/a_diag;
+		for (int i=1;i<n+1;i++){
+			for (int j=1;j<n+1;j++){
+
+				x[i*(n+2)+j] = (1-(a_down*u[(i-1)*(n+2)+j]+a_left*u[i*(n+2)+j-1]+a_right*u[i*(n+2)+j+1]+a_up*u[(i+1)*(n+2)+j]))/a_diag;
+			}
+			//std::cout << x[i] << std::endl;
 		}
 		// set start vector for next iteration
 		u = x;
@@ -34,6 +69,20 @@ std::vector<float> jacobi(int n, float v, int iter){
 }
 
 int main() {
+	std::ofstream fs;
+	int n = 25;	
+	float h =(float)1/(n+1);
+	std::vector<float> res = gs(n,50,200);
+	std::string name = "25_50_result.dat";
+	fs.open(name);
+	for(int i=0; i<(n+2); i++){
+		if (i != 0){
+			fs << std::endl;
+		}
+		for (int j=0; j<(n+2); j++){
+			fs << i*h << " " << j*h << " " << res[i*(n+2)+j] << std::endl;
+		}	
+	}
 	
-	jacobi(10,10,10);
+	fs.close();
 }
