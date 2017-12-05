a62a341c32e3603e33d1c0c1212475bea5078fb2
jong42 <jonasguetter@gmx.de>
2017-12-05 16:15:41 +0100

    Update A6.cpp

--- Wissenschaftliches Rechnen 1/A6.cpp
+++ Wissenschaftliches Rechnen 1/A6.cpp
@@ -17,7 +17,13 @@ float scalar_vectorized (std::vector<float> a, std::vector<float> b){
 	float c = 0.0;
 
 	// Unterteilung der Daten in Blöcke von je 4 Elementen
-	//test git
+	// Datentyp: __m128 für float
+	//Here is a simple code on how to use SSE in order to compute the square root of 4 float 
+	//in a single operation using the _mm_sqrt_ps function.
+	
+	  float a[] __attribute__ ((aligned (16))) = { 41982.,  81.5091, 3.14, 42.666 };                                                                                                                                 
+  	  __m128* ptr = (__m128*)a;                                                                                                                                                                                      
+  	  __m128 t = _mm_sqrt_ps(*ptr);
 	
 	return c;
 
