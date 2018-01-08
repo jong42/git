4806f9df3d56ae577bf885e39218ac4ee8318ce1
jong42 <jonasguetter@gmx.de>
2018-01-09 00:06:48 +0100

    finished A9

--- Wissenschaftliches Rechnen 1/A9.cpp
+++ Wissenschaftliches Rechnen 1/A9.cpp
@@ -4,6 +4,10 @@
 #include <cmath>
 #include <xmmintrin.h>
 #include "pmmintrin.h"
+#include <chrono>
+
+using std::chrono::system_clock;
+using std::chrono::duration;
 
 std::vector<float> gs_upwind(int n, float v, int iter){
 	// Initialize variables
@@ -57,7 +61,7 @@ std::vector<float> jacobi_upwind(int n, float v, int iter){
 
 				x[i*(n+2)+j] = (1-(a_upup*u[(i-2)*(n+2)+j]+a_up*u[(i-1)*(n+2)+j]+a_leftleft*u[i*(n+2)+j-2]+a_left*u[i*(n+2)+j-1]))/a_diag;
 			}
-			//std::cout << x[i] << std::endl;
+			
 		}
 		// set start vector for next iteration
 		std::swap(x,u);
@@ -82,11 +86,12 @@ float * jacobi_upwind_sse(int n, float v, int iter){
 	float a_upup = -1/(h*h);
 	float * x;
 	float * u;
+	float * u_leftleft;
+	float * u_left;
 	posix_memalign((void**)&x, 16, sizeof(float)*((n+4)*(n+4)));
 	posix_memalign((void**)&u, 16, sizeof(float)*((n+4)*(n+4)));
-	//TODO
-	//posix_memalign((void**)&u_vec_leftleft, 16, sizeof(float)*4);
-	//posix_memalign((void**)&u_vec_left, 16, sizeof(float)*4);
+	posix_memalign((void**)&u_leftleft, 16, sizeof(float)*4);
+	posix_memalign((void**)&u_left, 16, sizeof(float)*4);
 
 	__m128 x_vec;
 	__m128 u_vec_upup;
@@ -97,6 +102,14 @@ float * jacobi_upwind_sse(int n, float v, int iter){
 	__m128 up_vec;
 	__m128 leftleft_vec;
 	__m128 left_vec;
+
+	__m128 a_upup_vec = _mm_load_ps1(&a_upup);
+	__m128 a_up_vec = _mm_load_ps1(&a_up);
+	__m128 a_left_vec = _mm_load_ps1(&a_left);
+	__m128 a_leftleft_vec = _mm_load_ps1(&a_leftleft);
+	__m128 a_diag_vec = _mm_load_ps1(&a_diag);
+	__m128 ones_sse = _mm_set_ps1(1.0);
+
 	// Begin iterations
 	for (int w=0;w<iter;w++){
 		for (int i=2;i<n+4;i++){
@@ -105,29 +118,35 @@ float * jacobi_upwind_sse(int n, float v, int iter){
 				// load old matrix values needed for computing the new vector
 				u_vec_upup = _mm_load_ps(&u[(i-2)*(n+4)+j]);	
 				u_vec_up = _mm_load_ps(&u[(i-1)*(n+4)+j]);
-				//TODO
-				//u_vec_leftleft = _mm_load_ps(&u[i*(n+4)+j-2]);
-				//u_vec_left = _mm_load_ps(&u[i*(n+2)+j-1]);
+				
+				u_leftleft[0] = u[i*(n+4)+j-2];
+				u_leftleft[1] = u[i*(n+4)+j-2+1];
+				u_leftleft[2] = u[i*(n+4)+j-2+2];
+				u_leftleft[3] = u[i*(n+4)+j-2+3];
+
+				u_left[0] = u[i*(n+2)+j-1];
+				u_left[1] = u[i*(n+2)+j-1+1];
+				u_left[2] = u[i*(n+2)+j-1+2];
+				u_left[3] = u[i*(n+2)+j-1+3];
+
+				u_vec_leftleft = _mm_load_ps(&u_leftleft[0]);
+				u_vec_left = _mm_load_ps(&u_left[0]);
 
 
 				// Calculate new vector of values
-				//upup_vec = _mm_mul_ps(a_upup_vec,u_vec_upup);
-				//up_vec = _mm_mul_ps(a_up_vec,u_vec_up);
-				//leftleft_vec = _mm_mul_ps(a_leftleft_vec,u_vec_leftleft);
-				//left_vec = _mm_mul_ps(a_left_vec,u_vec_left); 
-				//x_vec = _mm_add_ps(_mm_add_ps(up_vec,upup_vec), _mm_add_ps(left_vec,leftleft_vec));
-				//x_vec = _mm_sub_ps (ones_sse,x_vec);
-				//x_vec = _mm_div_ps(x_vec,a_diag_vec);
+				upup_vec = _mm_mul_ps(a_upup_vec,u_vec_upup);
+				up_vec = _mm_mul_ps(a_up_vec,u_vec_up);
+				leftleft_vec = _mm_mul_ps(a_leftleft_vec,u_vec_leftleft);
+				left_vec = _mm_mul_ps(a_left_vec,u_vec_left); 
+				x_vec = _mm_add_ps(_mm_add_ps(up_vec,upup_vec), _mm_add_ps(left_vec,leftleft_vec));
+				x_vec = _mm_sub_ps (ones_sse,x_vec);
+				x_vec = _mm_div_ps(x_vec,a_diag_vec);
 
 				// Write new values back to x
-				//_mm_store_ps(&x[i*(n+2)+j+inner_loop_iterations*4],x_vec);
-				
-				x[i*(n+4)+j] = (1-(a_upup*u[(i-2)*(n+4)+j]+a_up*u[(i-1)*(n+4)+j]+a_leftleft*u[i*(n+4)+j-2]+a_left*u[i*(n+4)+j-1]))/a_diag;
-				x[i*(n+4)+j+1] = (1-(a_upup*u[(i-2)*(n+4)+j+1]+a_up*u[(i-1)*(n+4)+j+1]+a_leftleft*u[i*(n+4)+j-2+1]+a_left*u[i*(n+4)+j-1+1]))/a_diag;
-				x[i*(n+4)+j+2] = (1-(a_upup*u[(i-2)*(n+4)+j+2]+a_up*u[(i-1)*(n+4)+j+2]+a_leftleft*u[i*(n+4)+j-2+2]+a_left*u[i*(n+4)+j-1+2]))/a_diag;
-				x[i*(n+4)+j+3] = (1-(a_upup*u[(i-2)*(n+4)+j+3]+a_up*u[(i-1)*(n+4)+j+3]+a_leftleft*u[i*(n+4)+j-2+3]+a_left*u[i*(n+4)+j-1+3]))/a_diag;
+				_mm_store_ps(&x[i*(n+4)+j],x_vec);
+
 			}
-			//std::cout << x[i] << std::endl;
+			
 		}
 		// set start vector for next iteration
 		std::swap(x,u);
@@ -141,18 +160,39 @@ float * jacobi_upwind_sse(int n, float v, int iter){
 
 int main(){
 	std::ofstream fs;
-	int n = 400;	
+	int n = 1024;	
 	int v = 200;
 	float h =(float)1/(n+1);
-	float * res = jacobi_upwind_sse(n,50,20);
-	std::string name = "A9_result.dat";
-	fs.open(name);
-	for(int i=0; i<(n+2); i++){
+	auto start = system_clock::now();
+	float * res_sse = jacobi_upwind_sse(n,50,2000);
+	auto end = system_clock::now();
+	const double elapsed_seconds_sse = duration<double>(end - start).count();
+	start = system_clock::now();
+	std::vector<float> res_serial = jacobi_upwind(n,50,2000);
+	end = system_clock::now();
+	const double elapsed_seconds_serial = duration<double>(end - start).count();
+	std::string name_sse = "A9_result_sse.dat";
+	std::string name_serial = "A9_result_serial.dat";
+	std::cout << "Time taken serial program: " << elapsed_seconds_serial << std::endl;
+	std::cout << "Time taken with SSE: " << elapsed_seconds_sse << std::endl;
+	fs.open(name_sse);
+	for(int i=2; i<(n+4); i++){
 		if (i != 0){
 			fs << std::endl;
 		}
-		for (int j=0; j<(n+2); j++){
-			fs << i*h << " " << j*h << " " << res[i*(n+2)+j] << std::endl;
+		for (int j=2; j<(n+4); j++){
+			fs << i*h << " " << j*h << " " << res_sse[i*(n+4)+j] << std::endl;
+		}	
+	}
+	
+	fs.close();
+	fs.open(name_serial);
+	for(int i=2; i<(n+4); i++){
+		if (i != 0){
+			fs << std::endl;
+		}
+		for (int j=2; j<(n+4); j++){
+			fs << i*h << " " << j*h << " " << res_serial[i*(n+4)+j] << std::endl;
 		}	
 	}
 	
